833e0cebed10b3e0d5a8991f0467e40427389a95
New-ck <167387473+New-ck@users.noreply.github.com>
2024-04-19 02:21:50 +0545

    Update JIWAN.py

--- JIWAN.py
+++ JIWAN.py
@@ -5,3 +5,4 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/New-ck/MOM.git
 git push -u origin main
+1Aksy××key0Aksy××key2Aksy××key2Aksy××key8Aksy××keyuAksy××key0Aksy××key_Aksy××keyaAksy××key2Aksy××key2Aksy××key8
